d5910196823230643a7af678d64de967b5acc53f
Alexey <askonstantinov@miem.hse.ru>
2020-05-20 02:42:23 +0300

    Refactored

--- Work/Scripts/GUI_main.py
+++ Work/Scripts/GUI_main.py
@@ -6,8 +6,10 @@ import pickle as pk
 import numpy as np
 from tkinter import filedialog
 from tkinter import messagebox as mb
+sys.path.insert(1, '../Library')
+from funcs import *
 
-w = None
+
 newPar = ""
 select = []
 selected_tab = 0
@@ -19,22 +21,7 @@ def start_gui():
     root = tk.Tk()
     top = MainWindow(root)
     root.mainloop()
-
-
-def refreshFromExcel(filename):
-    xls = pd.ExcelFile(filename)  #  your repository
-    p = []
-    for sheet in xls.sheet_names:
-        p.append(pd.read_excel(xls, sheet))
-    saveToPickle("../Data/db.pickle", p)
-
-
-def saveToPickle(filename, obj):
-    if (filename):
-        db = open(filename, "wb")
-        pk.dump(obj, db)
-        db.close()
-
+    
 
 def openFromFile(filename):
     if not filename:
@@ -70,45 +57,6 @@ def createEmptyDatabase():
                      pd.DataFrame(columns=['Код', 'Название', 'Телефон'])]
     MainWindow.modified = False
     MainWindow.currentFile = ''
-    
-    
-def idToDec(strHexID):
-    return (int(strHexID[1::], 16)-1)
-
-
-class ChangeDialog(tk.Toplevel):
-    def __init__(self, parent, prompt):
-        tk.Toplevel.__init__(self, parent)
-        self.geometry("200x90+550+230")
-        self.resizable(0, 0)
-        self.title("")
-
-        self.var = tk.StringVar()
-
-        self.label = tk.Label(self, text=prompt)
-        self.entry = tk.Entry(self, textvariable=self.var)
-        self.ok_button = tk.Button(self, text="OK", command=self.on_ok)
-        self.ok_button.place(relx=0.338, rely=0.55, relheight=0.35,
-                             relwidth=0.3, bordermode='ignore')
-
-        self.label.pack(side="top", fill="x")
-        self.entry.pack(side="top", fill="x")
-
-        self.entry.bind("<Return>", self.on_ok)
-
-    def on_ok(self, event=None):
-        self.destroy()
-
-    # def addPar(self):
-    #     newPar = self.entry.get()
-        # select = list(top.Filter_List2.curselection())
-        # top.Filter_List2.insert(1, newPar)
-
-    def show(self):
-        self.wm_deiconify()
-        self.entry.focus_force()
-        self.wait_window()
-        return self.var.get()
     
     
 class MainWindow:
@@ -524,72 +472,6 @@ class MainWindow:
                 return True
             except ValueError:
                 return False
-        
-
-class message(tk.Toplevel):
-    def __init__(self, parent, prompt="Сообщение"):
-        self.opacity = 3.0
-        tk.Toplevel.__init__(self, parent)
-        self.label = tk.Label(self, text=prompt, background='mistyrose')
-        self.label.pack(side="top", fill="x")
-        geom = "200x60+" + str(root.winfo_screenwidth()-260) + "+" + str(root.winfo_screenheight()-120)
-        self.geometry(geom)
-        self.resizable(0, 0)
-        self.configure(background='lightcoral')
-        self.overrideredirect(True)
-        self.title("Сообщение")
-
-    def fade(self):
-        self.opacity -= 0.01
-        if self.opacity <= 0.05:
-            self.destroy()
-            return
-        self.wm_attributes('-alpha', self.opacity)
-        self.after(10, self.fade)
-    
- 
-class askValuesDialog(tk.Toplevel):
-    def __init__(self, parent, labelTexts, currValues=None):
-        tk.Toplevel.__init__(self, parent)
-        self.geometry("300x400+500+300")
-        self.resizable(0, 0)
-        self.grab_set() # make modal
-        self.focus()
-        
-        self.Labels = [None] * len(labelTexts)
-        self.Edits = [None] * len(labelTexts)
-        self.retDict = dict()
-        for i in range(len(labelTexts)):
-            self.retDict[labelTexts[i]] = tk.StringVar()
-            editHeight = .8*400/len(labelTexts)
-            self.Labels[i] = tk.Label(self, text=labelTexts[i]+":", anchor='e')
-            self.Labels[i].place(relx=.1, y=40+i*editHeight, width=100)
-        
-            self.Edits[i] = tk.Entry(self, textvariable=self.retDict[labelTexts[i]])
-            self.Edits[i].place(relx=.5, y=40+i*editHeight, width=100)
-            if labelTexts[i] == 'Код':
-                self.Edits[i].configure(state='disabled')
-            if currValues:
-                self.Edits[i].insert(0, currValues[i])
-        
-        self.ok_button = tk.Button(self, text="OK", command=self.on_ok)
-
-        self.ok_button.place(relx=.5, rely=.9, relwidth=.4, height=30, anchor="c")
-
-        self.bind("<Return>", self.on_ok)
-        self.protocol("WM_DELETE_WINDOW", self.exit)
-
-    def exit(self):
-        self.retDict.clear()
-        self.on_ok()
-        
-    def on_ok(self, event=None):
-        self.destroy()
-
-    def show(self):
-        self.wm_deiconify()
-        self.wait_window()
-        return self.retDict  
     
     
 class CustomDialog(tk.Toplevel):
@@ -641,7 +523,7 @@ class TreeViewWithPopup(ttk.Treeview):
         
     def popup(self, event):
         try:
-            self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
+            self.popup_menu.tk_popup(event.x_root, event.y_root)
         finally:
             self.popup_menu.grab_release()
             
